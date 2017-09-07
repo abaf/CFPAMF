a62b5e74974a0088c4c61e1f40113233c7149c24
Jian Liu <jian.lau@outlook.com>
2017-09-07 11:31:43 +0800

    create project to test query trade deduct interface with high frequencies

--- TradeDeductQueryTest/TradeDeductQueryTest/Program.cs
+++ TradeDeductQueryTest/TradeDeductQueryTest/Program.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace YcloansLogParser.Downloander
+namespace TradeDeductQueryTest
 {
-    public class Class1
+    class Program
     {
+        static void Main(string[] args)
+        {
+        }
     }
 }
